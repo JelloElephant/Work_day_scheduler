c10586696549b5a0d35ccc796ce0f5b23fe9f782
Jake Eik <eik995@hotmail.com>
2023-06-25 18:40:09 -0500

    Bug fixing

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+};
+
+var currTime = new Date();
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(currTime.toLocaleString(('en-US'), options));
+
+  $('#container').html(
+    [9, 10, 11, 12, 13, 14, 15, 16, 17].map((hour) => {
+      var timeRef = '';
+
+      var preVal = localStorage.getItem(`block-${hour}`) || '';
+
+      if (currTime.getHours() === hour) {
+        timeRef = 'current row time-block';
+      } else if (currTime.getHours() < hour) {
+        timeRef = 'future row time-block';
+      } else {
+        timeRef = 'past row time-block';
+      }
+
+      return `<div id='hour-${hour}' class='row ${timeRef}'>
+        <div class='col-2 col-md-1 hour text-center align-items-center justify-content-center'>${hour === 12 ? 12 : hour % 12}${hour / 12 >= 1 ? 'PM' : 'AM'}</div>
+        <textarea id='block-${hour}' class='col-8 col-md-10 task' rows='3'>${preVal}</textarea>
+        <button id='btn-${hour}' class='btn saveBtn col-2 col-md-1' aria-label='save'>
+          <i class='fas fa-save' aria-hidden='true'></i>
+        </button>
+      </div>`;
+    }).join('')
+  );
+
+  $('.task').on('change', (text) => {
+    $(document).on('click', '.saveBtn, .saveBtn i', (btn) => {
+      var hourNum = btn.target.id ? btn.target.id.split('-')[1] : btn.target.parentElement.id.split('-')[1];
+      var updateText = `block-${hourNum}`;
+      var textUpdated = $(`#${updateText}`).val();
+      localStorage.setItem(updateText, textUpdated);
+    })
+  });
 });
